60012f6d544ab8bf009383baf0326f6b4aa3b654
Trọng Nghĩa <117697590+truongtrantrongnghia2110@users.noreply.github.com>
2023-06-29 23:14:07 +0700

    Create JS01.js

---
+++ bai3/JS01.js
@@ -0,0 +1,10 @@
+// Hàm kiểm tra số chẵn hay lẽ
+function kiemTraChanLe(so) {
+    if(so % 2 == 0)
+        return 'Chẵn'
+    else 
+        return 'Lẻ'
+}
+
+// Xuất ra kết quả
+console.log(kiemTraChanLe(100));
